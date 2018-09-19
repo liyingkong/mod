a306f1f220d06ca78e3559a48dea353f8383f807
lyk <1437524435@qq.com>
2018-09-19 10:34:18 +0800

    修改本地数据库到251服务器数据库

--- src/test/java/com/senlang/ModApplicationTests.java
+++ src/test/java/com/senlang/ModApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FaceApplicationTests {
+public class ModApplicationTests {
 
     @Test
     public void contextLoads() {
